5b68b69fd78f9fe39241ba4dbcd3829bc77c7904
JorgeDam26 <jherreram13@educastillalamancha.es>
2024-10-21 10:35:34 +0200

    ejercicio calculadora simple incompleta

--- tema-2/ejercicios/ejercicio-pasword/app/src/main/java/com/example/ejercicio_pasword/PrimeraPantalla.kt
+++ tema-2/ejercicios/ejercicio-pasword/app/src/main/java/com/example/ejercicio_pasword/PrimeraPantalla.kt
@@ -1,17 +1,20 @@
 package com.example.ejercicio_pasword
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.inputmethod.InputBinding
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ejercicio_pasword.databinding.ActivityPrimeraPantallaBinding
 
 class PrimeraPantalla : AppCompatActivity() {
 
     companion object {
         val CONSTANTE_MENSAJE: String = "MENSAJE"
-        val CONSTANTE_TELEFONO: String = "TELEFONO"
+
     }
 
     private lateinit var binding: ActivityPrimeraPantallaBinding
@@ -19,12 +22,29 @@ class PrimeraPantalla : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_primera_pantalla)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityPrimeraPantallaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.validar.setOnClickListener(){
+
+            comprobarPassword(binding.textoPasword.text.toString())
+
+        }
+
+    }
+
+    fun comprobarPassword(pasword : String){
+
+        if(pasword.equals("abc123")){
+            val intent = Intent(this, SegundaPantalla::class.java)
+            intent.putExtra(CONSTANTE_MENSAJE, binding.textoPasword.text.toString())
+            startActivity(intent)
+
+        }else {
+
+            Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show()
+
         }
     }
 }
--- tema-2/ejercicios/ejercicio-pasword/app/src/main/java/com/example/ejercicio_pasword/SegundaPantalla.kt
+++ tema-2/ejercicios/ejercicio-pasword/app/src/main/java/com/example/ejercicio_pasword/SegundaPantalla.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ejercicio_pasword.databinding.ActivitySegundaPantallaBinding
 
 class SegundaPantalla : AppCompatActivity() {
 
@@ -13,12 +14,10 @@ class SegundaPantalla : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_segunda_pantalla)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivitySegundaPantallaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }

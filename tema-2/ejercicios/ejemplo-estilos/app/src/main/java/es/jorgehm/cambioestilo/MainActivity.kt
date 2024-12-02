cdf6748adc6bdfc0283e45c0d6cea6aa6664c894
JorgeDam26 <jorgehm260505@gmail.com>
2024-12-02 10:32:49 +0100

    avance

--- tema-2/ejercicios/ejemplo-estilos/app/src/main/java/es/jorgehm/cambioestilo/MainActivity.kt
+++ tema-2/ejercicios/ejemplo-estilos/app/src/main/java/es/jorgehm/cambioestilo/MainActivity.kt
@@ -1,20 +1,30 @@
 package es.jorgehm.cambioestilo
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val modoOscuro = AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+      // val sharedPreferences = PreferenceManager.getDefaultSharedPreferencesName(this)
+       // val themeName = sharedPreferences.getString("")
+    }
+
+    fun switchToDarkMode(isDarkModeEnabled: Boolean) {
+        if (isDarkModeEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
+        recreate()
     }
 }
